d59d758419f184f6b95310bd8ac1269626ab1272
Brianna Giunta <brig_oowee@Briannas-MacBook-Pro.local>
2021-06-01 17:49:09 -0400

    added tech logos

--- src/components/tech.js
+++ src/components/tech.js
@@ -1,7 +1,24 @@
 const Tech = () =>{
     return(
-        <div>
-
+        <div className = 'tech-container'>
+            <div className = 'tech'>
+                <img className = 'tech-pic' src = 'https://i.imgur.com/mkm5pGJ.png' />
+                <img className = 'tech-pic' src = 'https://i.imgur.com/vgeE8RC.png?1' />
+                <img className = 'tech-pic' src = 'https://i.imgur.com/wRDc6cL.png' />
+                <img className = 'tech-pic' src = 'https://i.imgur.com/T1nYC5Q.png' />
+                <img className = 'tech-pic' src = 'https://i.imgur.com/u2oEsnM.png' />
+                <img className = 'tech-pic' src = 'https://i.imgur.com/hLVXrDA.png?4' />
+            </div>
+            <div className = 'tech'>
+                <img className = 'tech-pic' src = 'https://i.imgur.com/Wrblf7i.jpg' />
+                <img className = 'tech-pic' src = 'https://i.imgur.com/ZlD2U6R.jpg' />
+                <img className = 'tech-pic' src = 'https://i.imgur.com/yskbwAo.png' />
+                <img className = 'tech-pic' src = 'https://i.imgur.com/fZAUnlt.jpg' />
+                <img className = 'tech-pic' src = 'https://i.imgur.com/krShlNZ.png' />
+                <img className = 'tech-pic' src = 'https://i.imgur.com/MXwvbJ7.png' />
+                <img className = 'tech-pic' src = 'https://i.imgur.com/O4QM5zv.png' />
+                
+            </div>
         </div>
     )
 }
