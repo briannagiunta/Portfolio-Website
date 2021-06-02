58577feb31e3aaa327d13803373bfe4c24b4f1fb
Brianna Giunta <brig_oowee@Briannas-MacBook-Pro.local>
2021-06-01 20:14:16 -0400

    added about me and contact links/icons

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Projects from './components/projects'
 function App() {
   return (
     <div className="App">
+      {/* <a href='#about-container'>About</a> */}
       <Header />
       <Tech />
       <About />
--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,7 @@
+import {useState} from 'react'
+
 const Header = () =>{
+    const [show, setShow] = useState(null)
     return(
         <div className = 'header-container'>
             <div className = 'header'>
@@ -8,7 +11,34 @@ const Header = () =>{
                         <div className = 'title'>Fullstack Developer</div>
                     </div>
                     <div className = 'contact'>
-                        Contact Me
+                        <div>Contact</div>
+                        <div className = 'icon-container'>
+                            <a href='https://www.linkedin.com/in/brianna-giunta/' target='_blank' onMouseEnter={()=>{setShow('Linkedin')}} onMouseLeave={()=>{setShow(null)}}>
+                                <img className = 'icon' src = 'https://i.imgur.com/MKS7B99.png?1'/>
+                                {show === 'Linkedin' && 
+                                <div className='icon-text'>Linkedin</div>
+                                }
+                            </a>
+                            <a href='https://www.linkedin.com/in/brianna-giunta/' target='_blank' onMouseEnter={()=>{setShow('Github')}} onMouseLeave={()=>{setShow(null)}}>
+                                <img className = 'icon' src = 'https://i.imgur.com/dWvBa9F.png?1'/>
+                                {show === 'Github' && 
+                                <div className='icon-text'>Github</div>
+                                }
+                            </a>
+                            <a href='mailto:briannagiunta@gmail.com?subject=Mail from Portfolio Website' target='_blank' onMouseEnter={()=>{setShow('Email')}} onMouseLeave={()=>{setShow(null)}}>
+                                <img className = 'icon' src = 'https://i.imgur.com/9XaQAga.png?2'/>
+                                {show === 'Email' && 
+                                <div className='icon-text'>Email</div>
+                                }
+                            </a>
+                            <a href='https://drive.google.com/file/d/1tEvKeAVjdXr3i5OBQwnlOqRw1QxYHkJy/view?usp=sharing' target='_blank' onMouseEnter={()=>{setShow('Resume')}} onMouseLeave={()=>{setShow(null)}}>
+                                <img className = 'icon' src = 'https://i.imgur.com/JY8aait.jpg?2'/>
+                                {show === 'Resume' && 
+                                <div className='icon-text'>Resume</div>
+                                }
+                            </a>
+
+                        </div>
                         
                     </div>
                 </div>
